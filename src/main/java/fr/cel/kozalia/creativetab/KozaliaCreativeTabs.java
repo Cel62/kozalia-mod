d6cf554dfd7c7be32c7800b7993cf1c8b0b05911
Cel <celcraftpro@gmail.com>
2022-05-31 20:43:56 +0200

    Commit #1 : I add the first main blocks of the mod : The Nitrite/Artrite/Terralium/Horium/Kozalium Blocks ! And of course I changed the Creative Tab's logo for the Kozalium Block who is more appropriated (instead of the apple :joy:)

--- src/main/java/fr/cel/kozalia/creativetab/KozaliaCreativeTabs.java
+++ src/main/java/fr/cel/kozalia/creativetab/KozaliaCreativeTabs.java
@@ -1,7 +1,7 @@
 package fr.cel.kozalia.creativetab;
 
+import fr.cel.kozalia.init.ModBlocks;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,9 +10,15 @@ public class KozaliaCreativeTabs {
 
     public static final CreativeTabs KOZALIA = new CreativeTabs(CreativeTabs.getNextID(), "kozalia") {
         @SideOnly(Side.CLIENT)
-        @Override
         public Item getTabIconItem() {
-            return Items.APPLE;
+            return Item.getItemFromBlock(ModBlocks.kozalium_block);
+        }
+    };
+
+    public static final CreativeTabs SKYFACTION = new CreativeTabs(CreativeTabs.getNextID(), "skyfaction") {
+        @SideOnly(Side.CLIENT)
+        public Item getTabIconItem() {
+            return null;
         }
     };
 
